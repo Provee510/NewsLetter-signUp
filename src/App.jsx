b37e5cf4dc1cf85155b85c722a0a002798ecdb4f
provee510 <providencecharles@yahoo.com>
2024-10-02 02:27:15 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Thanks from './components/Thanks';
 
 export const App = () => {
   return (
-   <div className="">
+   <div className=" ">
     <NewsLetter/>
     {/* <Thanks/> */}
-
+ <br /> <br /> <br />
     <div class="attribution flex items-center justify-center">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
     Coded by <a href="#" className="text-red-700 font-bold" >&nbsp;PROVEE</a>.
--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -28,27 +28,27 @@ const Subscribe = () => {
     setError('');  // Clear any error
   };
 
-  // If the form is submitted, show the Thanks component
+  // submitted show hanks component
   if (isSubmitted) {
-    return <Thanks email={email} handleDismiss={handleDismiss} />;  // Pass the email and handleDismiss to Thanks component
+    return <Thanks email={email} handleDismiss={handleDismiss} />;  component
   }
 
   return (
-    <div className="general-container min-h-screen flex flex-col lg:flex-row  w-[390px] lg:w-full max-w-none justify-center bg-white lg:bg-gray-800 lg:p-10 px-0">
+    <div className="general-container h-screen flex flex-col lg:flex-row  w-[390px] lg:w-full max-w-none justify-center bg-white lg:bg-gray-800 lg:p-10 px-0">
       
       {/* Right container */}
-      <div className="right-container w-full h-[50vh] lg:w-[32%] lg:h-auto flex items-center justify-center order-1 lg:order-2">
-        <div className="w-full lg:w-[530px] h-full lg:h-[593px] relative lg:top-[10px] bg-white flex items-center rounded-r-3xl">
-          <img src={right} alt="Newsletter" className="w-full lg:w-[390px] h-full lg:rounded-3xl lg:h-[540px] object-cover rounded-bl-3xl rounded-br-3xl " />
+      <div className="right-container w-full h-[50vh] lg:w-[32%] lg:h-auto flex items-center justify-center order-1 lg:order-2 relative lg:top-[10px] ">
+        <div className="w-full lg:w-[530px] h-full lg:h-[579px] relative lg:top-[10px] bg-white flex items-center rounded-r-3xl">
+          <img src={right} alt="Newsletter" className="w-[500px] lg:w-[390px] h-full lg:rounded-3xl lg:h-[540px] object-cover rounded-bl-3xl rounded-br-3xl " />
         </div>
       </div>
-
+      
       {/* Left container */}
       <div className="left-container w-full lg:w-[40%] h-full bg-white mt-5 order-2 lg:order-1 lg:rounded-l-3xl">
         <div className="lg:py-20 p-5 lg:p-10">
           <h1 className="lg:text-5xl text-4xl font-bold text-darkSlateGrey">Stay updated!</h1>
           <div>
-            <p className="p-7 relative right-5 text-darkSlateGrey">Join 60,000+ product managers receiving monthly <br /> updates on:</p>
+            <p className="p-7 relative right-5 text-darkSlateGrey">Join 60,000+ product managers receiving monthly  updates on:</p>
             <div className="flex gap-3 p-2">
               <img src={check} alt="" className="h-5 w-5" />
               <p className="text-sm text-darkSlateGrey">Product discovery and building what matters</p>
@@ -75,7 +75,7 @@ const Subscribe = () => {
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}  // Update email state on input change
                   placeholder="email@company.com"
-                  className={`p-[14px] w-full rounded-md border ${error ? 'border-red-500' : 'border-gray-400'} hover:border-black`}
+                  className={`p-[14px] w-[370px] lg:w-full relative right-3 lg:right-0 rounded-md border ${error ? 'border-red-500' : 'border-gray-400'} hover:border-black`}
                 />
 
                 {/* Error message */}
@@ -87,7 +87,7 @@ const Subscribe = () => {
               </div>
 
               {/* Submit button */}
-              <div className="w-full bg-darkSlateGrey hover:bg-red-500 flex items-center justify-center rounded-md p-[14px]">
+              <div className="w-[370px] lg:w-full bg-darkSlateGrey hover:bg-red-500  flex items-center justify-center rounded-md p-[14px] relative right-3 lg:right-0">
                 <button type="submit" className="text-white w-full">
                   Subscribe to monthly newsletter
                 </button>
